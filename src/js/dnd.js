703d8a55a95fa0458481a8e19200fe54e1a800a7
Sergey Zhitkevich <horrorerror92@gmail.com>
2018-12-02 17:04:06 +0300

    dnd almost done

--- src/js/dnd.js
+++ src/js/dnd.js
@@ -1,10 +1,41 @@
 function dnd() {
 
-console.log('dnd in dev');
+  const source = document.querySelector('.myfriends');
+  const target = document.querySelector('.choicelist');
+  
+          
+  makeDnD([source, target]);
 
+  function makeDnD(zones) {
+  let currentDrag;
+
+    zones.forEach(zone => {
+      zone.addEventListener('dragstart', (e) => {
+        currentDrag = { source: zone, node: e.target };
+      });
+
+      zone.addEventListener('dragover', (e) => {
+        e.preventDefault();
+      });
+
+      zone.addEventListener('drop', (e) => {    
+        if (currentDrag) {
+          e.preventDefault();
+            zone.insertBefore(currentDrag.node, zone.lastElementChild);
+            if(e.currentTarget.classList.contains('choicelist')) {
+                  console.log('choise');
+               } else if(e.currentTarget.classList.contains('myfriends')){
+                  console.log('friends');
+                  
+               }
+
+         currentDrag = null;
+        }
+      });
+    })
+  }
 
 }
-
 
 export {
   dnd
